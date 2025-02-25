cd84d0053ebdbcf1647d77b4ecb4a71b6986fa54
Stephane Duguay (maison) <s@binarez.com>
2025-02-25 09:19:47 -0500

    Coords finales du terrain théorique et mise à jour de l'exporteur blender.

--- terrain/exporteur-blender.py
+++ terrain/exporteur-blender.py
@@ -8,13 +8,16 @@ def QuatVersDegree2D(q) -> float:
     q.normalize()
     return math.degrees(q.to_euler("XYZ")[2])
 
+def MmToMeters(mm : float) -> float:
+    return mm * 0.001
+
 prefixesInteressants = tuple(["b_", "r_"])
 
 objets = {} # liste de dictionnaires
 for obj in bpy.data.objects:
     if obj.name.startswith(prefixesInteressants):
         position, rotation, scale = obj.matrix_world.decompose()
-        objets[obj.name] = {'pos': [position.x, position.y], 'rot': QuatVersDegree2D(rotation)}
+        objets[obj.name] = {'pos': [MmToMeters(position.x), MmToMeters(position.y)], 'rot': QuatVersDegree2D(rotation)}
 
 dossierMaison = Path.home()
 with open(dossierMaison / 'terrain.json', 'w', encoding='utf-8') as fichier:
