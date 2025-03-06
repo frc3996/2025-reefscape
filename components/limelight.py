d381d2f3424c79fca10b34ce5dab68471cdb90db
Marc-Andre Rioux <marcandrioux@gmail.com>
2025-03-05 21:09:26 -0500

    Megatag2

--- components/limelight.py
+++ components/limelight.py
@@ -15,7 +15,6 @@ class LimeLightVision:
 
     def __init__(self, name: str):
         self.cameraName = name
-        self.visionPose = Pose2d()
 
     def light_pipeline(self):
         LimelightHelpers.set_LED_to_pipeline_control(self.cameraName)
@@ -29,38 +28,16 @@ class LimeLightVision:
     def light_on(self):
         LimelightHelpers.set_LED_to_force_on(self.cameraName)
 
-    def execute(self):
-        pass
-
-    def getVisionMesurementMT1(self):
-        # Getting blue megatag2 pose, both teams are in blue-team space
-        poseEstimate = LimelightHelpers.get_botpose_estimate_wpiblue(self.cameraName)
-        if poseEstimate is None:
-            # print(self.__cameraName + ": no pose estimate")
-            return None
-        elif poseEstimate.tag_count == 0:
-            # print(self.__cameraName + ": pose estimate with no tags")
-            return None
-        else:
-            return (
-                poseEstimate.pose,
-                utils.fpga_to_current_time(
-                    poseEstimate.timestamp_seconds
-                ),  # TODO nécessaire??
-                self._get_dynamic_std_devs(poseEstimate),
-            )
-
-    @feedback
-    def getVisionPosition(self):
-        return self.visionPose
+    def setRobotOrientation(self, robotPose: Pose2d):
+        LimelightHelpers.set_robot_orientation(
+            self.cameraName, robotPose.rotation().degrees(), 0, 0, 0, 0, 0
+        )
 
     def getVisionMesurement(
         self,
-        robotOrientation: Rotation2d,
     ) -> tuple[Pose2d, float, tuple[float, float, float]] | None:
 
         assert self.cameraName != ""
-
         # Getting blue megatag2 pose, both teams are in blue-team space
         poseEstimate = LimelightHelpers.get_botpose_estimate_wpiblue(self.cameraName)
         if poseEstimate is None:
@@ -70,7 +47,6 @@ class LimeLightVision:
             # print(self.__cameraName + ": pose estimate with no tags")
             return None
         else:
-            self.visionPose = poseEstimate.pose
             return (
                 poseEstimate.pose,
                 utils.fpga_to_current_time(
