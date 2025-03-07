ba4d0e9aade99e590a1cf27ce57c7cf28eb13163
Stephane Duguay <s@binarez.com>
2025-03-07 02:29:53 -0500

    RunAuto sequence: move, lift, shoot.

--- autonomous/auto_modes.py
+++ autonomous/auto_modes.py
@@ -1,26 +1,46 @@
+import wpimath.units as units
+
 from typing import override
 
-from magicbot import state
+from magicbot import state, tunable
 from magicbot.state_machine import AutonomousStateMachine
 
-from components.robot_actions import ActionCycleAutonomous
 from components.swervedrive import SwerveDrive
-import common.tools
+from components.robot_actions import ActionShoot
+from components.lift import LiftTarget
 
 class RunAuto(AutonomousStateMachine):
     DEFAULT: bool = True
     MODE_NAME: str = "RunAuto"
     PATH_NAME: str = "RunAuto"
-    actionCycleAutonomous: ActionCycleAutonomous
+
     drivetrain: SwerveDrive
+    actionShoot : ActionShoot
+
+    kDistanceToTravel = tunable(units.inchesToMeters(60)) # From CAD file
+    kTravelSpeed = tunable(0.6)
+    kLiftTarget = LiftTarget.L3
+
+    @override
+    def on_enable(self) -> None:
+        self.initialPose = self.drivetrain.getPose()
+        self.lockedDone = False
+        super().on_enable()
 
     @state(first=True)
     def run(self):
-        """
-        Just call the autonmous statemachine, then call drivetrain.drive like
-        in teleopPeriodic
+        if self.lockedDone:
+            return
+        distanceTraveled = self.drivetrain.getPose().translation().distance(self.initialPose.translation())
+        if distanceTraveled >= self.kDistanceToTravel:
+            self.drivetrain.drive(0, 0, 0, True)
+            self.actionShoot.start(self.kLiftTarget, "move_lift_auto")
+            if not self.actionShoot.is_executing:
+                self.done()
+        else:
+            self.drivetrain.drive(self.kTravelSpeed, 0, 0, True)
 
-        Make sure the drive is executed at the end!
-        """
-        # self.actionCycleAutonomous.engage()
-        self.drivetrain.drive(0.2, 0, 0, True)
+    @override
+    def done(self) -> None:
+        self.lockedDone = True
+        super().done()
