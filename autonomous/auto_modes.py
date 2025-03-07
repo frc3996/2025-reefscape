4c3a7d45a867d93e4eb759b881d9ee166f775d97
Stephane Duguay <s@binarez.com>
2025-03-07 09:31:34 -0500

    Better automode RunAuto

--- autonomous/auto_modes.py
+++ autonomous/auto_modes.py
@@ -5,9 +5,11 @@ from typing import override
 from magicbot import state, tunable
 from magicbot.state_machine import AutonomousStateMachine
 
-from components.swervedrive import SwerveDrive
+from components.swervedrive import SwerveDrive, kMaxSpeed
 from components.robot_actions import ActionShoot
 from components.lift import LiftTarget
+
+kRunAutoSpeedFactor : float = 0.5
 
 class RunAuto(AutonomousStateMachine):
     DEFAULT: bool = True
@@ -18,29 +20,36 @@ class RunAuto(AutonomousStateMachine):
     actionShoot : ActionShoot
 
     kDistanceToTravel = tunable(units.inchesToMeters(60)) # From CAD file
-    kTravelSpeed = tunable(0.6)
     kLiftTarget = LiftTarget.L3
 
     @override
     def on_enable(self) -> None:
+        print("Run auto mode: on_enable recording initial pose")
         self.initialPose = self.drivetrain.getPose()
-        self.lockedDone = False
         super().on_enable()
 
     @state(first=True)
-    def run(self):
-        if self.lockedDone:
-            return
+    def move(self):
+        print("Run auto mode: moving state")
         distanceTraveled = self.drivetrain.getPose().translation().distance(self.initialPose.translation())
         if distanceTraveled >= self.kDistanceToTravel:
-            self.drivetrain.drive(0, 0, 0, True)
+            self.next_state("shoot")
+        else:
+            self.drivetrain.drive(kMaxSpeed * kRunAutoSpeedFactor, 0, 0, True)
+
+    @state
+    def shoot(self):
+        print("Run auto mode: shooting state" )
+        if not self.actionShoot.is_executing:
+            print("Run auto mode: starting lift")
             self.actionShoot.start(self.kLiftTarget, "move_lift_auto")
+        else:
+            print("Run auto mode: keep shoot going")
+            self.actionShoot.engage()
             if not self.actionShoot.is_executing:
                 self.done()
-        else:
-            self.drivetrain.drive(self.kTravelSpeed, 0, 0, True)
 
     @override
     def done(self) -> None:
-        self.lockedDone = True
+        print("Run auto mode: we're done")
         super().done()
